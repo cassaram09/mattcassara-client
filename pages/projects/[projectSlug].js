8162709b3447e961c385d04382554574f20adb41
cassaram09 <cassaram09@gmail.com>
2020-11-19 09:37:23 -0500

    refactor animations and data fetching for project detail

--- pages/projects/[projectSlug].js
+++ pages/projects/[projectSlug].js
@@ -1,65 +1,43 @@
 import styles from "../../assets/styles/pages/project.module.scss";
-import axios from "axios";
+import API from "../../utils/api";
 import { _class } from "../../utils/helpers";
 import Title from "../../components/Title";
-import { motion } from "framer-motion";
+import Reveal from "../../components/Reveal";
+
 const cl = _class(styles, "project");
 
 Project.propTypes = {
-  project: PropTypes.object,
+  page: PropTypes.object,
 };
 
 Project.defaultProps = {
-  project: {},
+  page: {},
 };
 
-export default function Project({ project }) {
-  const renderImage = () => {
-    const variants = {
-      hidden: { opacity: 0, y: 50 },
-      visible: { opacity: 1, y: 0, transition: { delay: 0.5 } },
-    };
+export default function Project({ page }) {
+  const renderImage = () => (
+    <Reveal className={cl("hero_image")} preset={"fadeDown"}>
+      <div
+        style={{
+          backgroundImage: `url(${page.featured_image.url})`,
+        }}
+        role="img"
+        aria-label={"alt"}
+      />
+    </Reveal>
+  );
 
-    return (
-      <motion.div
-        className={cl("hero_image")}
-        variants={variants}
-        initial={"hidden"}
-        animate={"visible"}
-      >
-        <div
-          style={{
-            backgroundImage: `url(${project.featured_image.url})`,
-          }}
-          role="img"
-          aria-label={"alt"}
-        />
-      </motion.div>
-    );
-  };
-
-  const renderContent = () => {
-    const variants = {
-      hidden: { opacity: 0, y: 50 },
-      visible: { opacity: 1, y: 0, transition: { delay: 1 } },
-    };
-    return (
-      <motion.div
-        className={cl("content")}
-        variants={variants}
-        initial={"hidden"}
-        animate={"visible"}
-      >
-        <div dangerouslySetInnerHTML={{ __html: project.content }} />
-      </motion.div>
-    );
-  };
+  const renderContent = () => (
+    <Reveal className={cl("content")} preset={"fadeUp"}>
+      <div dangerouslySetInnerHTML={{ __html: page.content }} />
+    </Reveal>
+  );
 
   return (
     <main className={cl("")}>
       <div className={cl("container")}>
         <div className={cl("heading")}>
-          <Title title={project.title} />
+          <Title title={page.title} />
         </div>
 
         {renderImage()}
@@ -70,27 +48,25 @@ export default function Project({ project }) {
   );
 }
 
-export async function getStaticPaths() {
-  const res = await axios.get(`${process.env.API_URL}/projects`);
+export const getServerSideProps = async (ctx) => {
+  const { projectBySlug } = await new API().graphql({
+    query: `
+      query GetProject {
+        projectBySlug(slug: "${ctx.params.projectSlug}") {
+          id
+          title
+          content
+          featured_image {
+            url
+          }
+        }
+      }
+      `,
+  });
+
   return {
-    paths: res.data.map((project) => ({
-      params: { projectSlug: project.slug },
-    })),
-    fallback: false,
+    props: {
+      page: projectBySlug,
+    },
   };
-}
-
-export async function getStaticProps(ctx) {
-  try {
-    const res = await axios.get(
-      `${process.env.API_URL}/projects/${ctx.params.projectSlug}`
-    );
-
-    return {
-      props: { project: res.data },
-    };
-  } catch (e) {
-    console.log(e);
-    return { props: {} };
-  }
-}
+};
