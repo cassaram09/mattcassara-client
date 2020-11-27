9c6e47e56f1fc42e31b5fa5a6a0bf908e74c5fdb
cassaram09 <cassaram09@gmail.com>
2020-11-27 10:41:53 -0500

    always scroll window to top on load

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-import React from "react";
-import App from "next/app";
+import { useEffect } from "react";
 import "../utils/ProxyPolyfill";
 import "../assets/styles/main.scss";
 import { motion, AnimatePresence } from "framer-motion";
@@ -7,52 +6,50 @@ import Layout from "../components/Layout";
 import SiteLoader from "../components/SiteLoader";
 import API from "../utils/api";
 
-export default class _App extends App {
-  static propTypes = {
-    nav: PropTypes.object,
-  };
+App.propTypes = {
+  nav: PropTypes.object,
+};
 
-  static getInitialProps = async ({ Component, ctx }) => {
-    const data = await new API().graphql({
-      query: `
-        query GetMenu{
-          menu {
-            menu_items {
-              title
-              path
-            }
-          }
-          global {
-            github_url
-            linkedin_url
+App.getInitialProps = async ({ Component, ctx }) => {
+  const data = await new API().graphql({
+    query: `
+      query GetMenu{
+        menu {
+          menu_items {
+            title
+            path
           }
         }
-        `,
-    });
+        global {
+          github_url
+          linkedin_url
+        }
+      }
+      `,
+  });
 
-    return { nav: data.menu, global: data.global };
-  };
+  return { nav: data.menu, global: data.global };
+};
 
-  render() {
-    const { Component, pageProps, router, nav, global } = this.props;
+export default function App({ Component, pageProps, router, nav, global }) {
+  useEffect(() => setTimeout(() => window.scroll(0, 0), 500), []);
 
-    return (
-      <div className="app">
-        <SiteLoader />
-        <Layout nav={nav} global={global}>
-          <AnimatePresence exitBeforeEnter>
-            <motion.div
-              key={router.route}
-              initial={{ opacity: 0 }}
-              exit={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              className="page"
-            >
-              <Component {...pageProps} key={router.route} />
-            </motion.div>
-          </AnimatePresence>
-        </Layout>
-      </div>
-    );
-  }
+  return (
+    <div className="app">
+      <SiteLoader />
+      <Layout nav={nav} global={global}>
+        <AnimatePresence exitBeforeEnter>
+          <motion.div
+            key={router.route}
+            initial={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="page"
+          >
+            <Component {...pageProps} key={router.route} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </div>
+  );
 }
