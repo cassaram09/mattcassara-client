1955a5c28c7cdd7040d8c1624a445d889cfcc1bd
cassaram09 <cassaram09@gmail.com>
2020-11-27 21:51:55 -0500

    adjust mobile nav size

--- components/Nav.js
+++ components/Nav.js
@@ -64,7 +64,7 @@ export default function Nav({ items, scrolled }) {
                 initial={"hidden"}
               >
                 <Link
-                  title={item.label}
+                  title={item.title}
                   path={item.path}
                   onClick={() => toggleNav(false)}
                 />
